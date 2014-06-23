8b4ce5ac2bceadfb7de219b5ba67c35105d0fcf8
agu <guohai@live.com>
2014-06-24 01:05:54 +0800

    ab

--- run.py
+++ run.py
@@ -3,19 +3,28 @@ __author__ = 'guohai@live.com'
 
 import sys
 import os
+import re
 import unittest
 import StringIO
 from core import HTMLTestRunner
 from util import excel
 
+#遍历文件夹
+def findCase(folder):
+    for root, dis, files in os.walk(folder):
+        for file in files:
+            print root+os.sep+file
 
 def loadSuite():
     casePath = sys.path[0] + os.sep + 'testcase'
+    #findCase(casePath)
+
     discover = unittest.defaultTestLoader.discover(casePath)
     suite = unittest.TestSuite()
     for test_suit in discover:
         for test_case in test_suit:
             suite.addTests(test_case)
+
     return suite
 
 def main():
