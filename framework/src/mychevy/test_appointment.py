0bef52e6ac930a9470a61c264e5c7f250ff99d59
guohai@live.com <guohai@live.com>
2014-08-06 13:29:25 +0800

    src testcase

--- framework/src/mychevy/test_appointment.py
+++ framework/src/mychevy/test_appointment.py
@@ -5,27 +5,21 @@ from time import sleep
 from framework.core import gvar,device
 
 
-class ContactsAndroidTests(unittest.TestCase):
+class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = gvar.driver
 
     def tearDown(self):
-        #self.driver.close_app()
+        #返回首页
         device.switchToHome(self,self.mainActivity)
 
 
-    def test_text(self):
-        sleep(15)
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
 
         self.driver.find_element_by_name(u'24小时服务站').click()
 
+        #有loading的webview页面，需要后期考虑
         sleep(10)
         self.driver.find_element_by_name(u'详情').click()
-
-        sleep(1)
-
-        # sleep(1)
-        # self.driver.find_element_by_name(u'取消').click()
-        # self.driver.keyevent(4)
-
--- framework/src/mychevy/test_meeting.py
+++ framework/src/mychevy/test_meeting.py
@@ -1,30 +1,20 @@
 # coding=utf-8
 __author__ = 'Administrator'
 import unittest
-from time import sleep
 from framework.core import gvar,device
 
 
-class ContactsAndroidTests(unittest.TestCase):
+class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = gvar.driver
 
     def tearDown(self):
-        #self.driver.close_app()
+        #返回首页
         device.switchToHome(self,self.mainActivity)
 
-    def test_text(self):
-        sleep(15)
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
 
         self.driver.find_element_by_name(u'聚乐会').click()
-
-        sleep(10)
-        self.driver.find_element_by_name(u'详情').click()
-
-        sleep(1)
-
-        # sleep(1)
-        # self.driver.find_element_by_name(u'取消').click()
-        # self.driver.keyevent(4)
 
