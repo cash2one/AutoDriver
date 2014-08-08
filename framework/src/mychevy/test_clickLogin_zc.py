b33a2557267560d47514aec2c3b9e4a5b665687b
farewell1990 <chun1990>
2014-08-08 11:07:39 +0800

    1

---
+++ framework/src/mychevy/test_clickLogin_zc.py
@@ -0,0 +1,24 @@
+__author__ = 'zhangchun'
+# coding=utf-8
+import unittest
+from time import sleep
+from framework.core import the,device
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android()
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+
+    def test_case(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        els=self.driver.find_elements_by_class_name('android.widget.ImageView')
+        els[3].click()
+        sleep(2)
+        self.driver.switch_to_alert()
+        self.assertEqual('.UserInformationActivity',self.driver.current_activity)
