dd385225948a9bbffef8866a5f7d3d3211f87381
farewell1990 <chun1990>
2014-08-11 17:51:50 +0800

    1

--- framework/src/mychevy/test_clickLogin_zc.py
+++ framework/src/mychevy/test_clickLogin_zc.py
@@ -3,11 +3,11 @@ __author__ = 'zhangchun'
 import unittest
 from time import sleep
 from framework.core import the,device
-
+#点击登录图标进入登录页面
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = the.android()
+        self.driver = the.android
 
     def tearDown(self):
         #返回首页
@@ -16,10 +16,9 @@ class TestCase(unittest.TestCase):
 
     def test_case(self):
         #每个测试用例，都需要把首页加入到变量mainActivity
-
         self.mainActivity = self.driver.current_activity
-        els=self.driver.find_elements_by_class_name('android.widget.ImageView')
-        els[3].click()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+
         sleep(2)
         self.driver.switch_to_alert()
         self.assertEqual('.UserInformationActivity',self.driver.current_activity)
