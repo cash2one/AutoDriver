86916f091ca482c1ac5ab72debf119db7908aa02
farewell1990 <chun1990>
2014-08-11 18:03:43 +0800

    1

--- framework/src/mychevy/test_meetingCall_zc.py
+++ framework/src/mychevy/test_meetingCall_zc.py
@@ -4,7 +4,7 @@ import unittest
 from time import sleep
 from framework.core import the,device
 
-
+#点击活动详情中的电话按钮，弹出提示框
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = the.android
@@ -18,16 +18,13 @@ class TestCase(unittest.TestCase):
         #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
         self.driver.find_element_by_name(u'聚乐会').click()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/imageView').click()
         sleep(2)
-
-        els=self.driver.find_elements_by_class_name('android.widget.TextView')
-        els[0].click()
-        sleep(2)
-        self.driver.find_elements_by_class_name('android.widget.Button')[0].click()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/content_phone').click()
         #点击拨打电话按钮，获取提示框
         self.driver.switch_to_alert()
         sleep(2)
-        txt=self.driver.find_elements_by_class_name('android.widget.TextView')[1].text
+        txt=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_msg').text
 
         print txt#打印提示内容
         self.assertEqual(u'没有找到蓝牙设备，请检查蓝牙连接是否正确',txt)
