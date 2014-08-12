9bd8f59a8f7082336294d8f3ec3e806c59523288
farewell1990 <chun1990>
2014-08-12 11:18:36 +0800

    1

---
+++ framework/src/mychevy/test_closeRemid_zc.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device
+from time import sleep
+#输入不存在的用户登录后，弹出提示框
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver =the.android
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000002')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
+
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_cancel').click()
+        #print(self.driver.current_activity)
+        self.assertTrue('.UserInformationActivity',self.driver.current_activity)
+
+
