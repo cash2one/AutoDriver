ca63503ddd5dc046b0ecec381802f7cfe7a00a1a
farewell1990 <chun1990>
2014-08-12 09:51:21 +0800

    1

---
+++ framework/src/mychevy/test_loginRemind_zc.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device
+from time import sleep
+#点击登录，输入信息点击确定，弹出提示框
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver =the.android
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+
+        sleep(2)
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000002')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
+        self.driver.switch_to_alert()
+        txt=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_msg').text
+        print txt
+        self.assertEqual(u'提醒：更换手机号会清空您原有的数据',txt)
+
+
