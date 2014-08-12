bbca2f31468e7741ef60e7d8976eaae496b40530
farewell1990 <chun1990>
2014-08-12 09:18:09 +0800

    1

--- framework/src/mychevy/test_notlogin_meeting_zc.py
+++ framework/src/mychevy/test_notlogin_meeting_zc.py
@@ -4,10 +4,10 @@ import unittest
 from time import sleep
 from framework.core import the,device
 
-
+#未登录时点击活动报名弹出登录页面
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = the.android
+        self.driver =the.android
 
     def tearDown(self):
         #返回首页
@@ -20,13 +20,12 @@ class TestCase(unittest.TestCase):
         self.driver.find_element_by_name(u'聚乐会').click()
         sleep(2)
 
-        els=self.driver.find_elements_by_class_name('android.widget.TextView')
-        els[0].click()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/imageView').click()
         sleep(2)
 
-        self.driver.find_elements_by_class_name('android.widget.Button')[1].click()#未登录点击报名进入登录页面
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/content_apply').click()#未登录点击报名进入登录页面
         self.driver.switch_to_alert()
 
-        print(self.driver.current_activity)
+       # print(self.driver.current_activity)
         self.assertEqual('.UserInformationActivity',self.driver.current_activity)
 
