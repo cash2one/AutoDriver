389d354f1ada5aa3b2cfca4ea27b4dacb114a871
farewell1990 <chun1990>
2014-08-13 09:55:49 +0800

    1

---
+++ framework/src/mychevy/test_faillogin_appointment_zc.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device
+from time import sleep
+#点击预约保养，点击提示框的确定按钮，成功预约
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+        sleep(3)
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000001')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
+        sleep(3)
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_cancel').click()
+        #登录失败
+        sleep(3)
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order').click()
+        self.driver.switch_to_alert()
+        txt=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_msg').text
+        print txt
+        self.assertTrue(u'登录失败：用户信息不正确' in txt)
