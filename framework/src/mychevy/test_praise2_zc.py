7f293730a70e5f4b0604ac5ac408b2869b827f81
farewell1990 <chun1990>
2014-08-13 13:24:40 +0800

    1

---
+++ framework/src/mychevy/test_praise2_zc.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device
+from time import sleep
+#点击详情，点击赞按钮，查看点赞数
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
+        sleep(3)
+        self.driver.find_element_by_name(u'详情').click()
+        sleep(3)
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_ok').click()
+        txt1=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_ok_number').text
+        self.driver.back()
+        txt2=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_ok_number').text
+        self.assertEqual(txt1,txt2)
+
