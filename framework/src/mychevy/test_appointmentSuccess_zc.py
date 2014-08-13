9e435c358252f0e68a3807d19b3ab8bf48441080
farewell1990 <chun1990>
2014-08-13 11:11:35 +0800

    1

--- framework/src/mychevy/test_appointmentSuccess_zc.py
+++ framework/src/mychevy/test_appointmentSuccess_zc.py
@@ -15,14 +15,14 @@ class TestCase(unittest.TestCase):
     def test_case1(self):
         #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
-        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
-        # sleep(2)
-        # self.driver.switch_to_alert()
-        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
-        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000002')
-        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
-        # self.driver.switch_to_alert()
-        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+        sleep(2)
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000002')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
         #登录成功
         sleep(3)
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order').click()
