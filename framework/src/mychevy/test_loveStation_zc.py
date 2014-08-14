f0f8471df6f40383fbd115d472ca8f98138c0a4a
farewell1990 <chun1990>
2014-08-14 15:32:05 +0800

    1

---
+++ framework/src/mychevy/test_loveStation_zc.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device
+from time import sleep
+#点击详情中的预约保养，选择指定时间，点击确定，预约成功
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
+        sleep(4)
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/rb_love_station').click()
+        txt=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_love_empty').text
+        self.assertEqual(u'目前还没有收藏的服务站',txt)
