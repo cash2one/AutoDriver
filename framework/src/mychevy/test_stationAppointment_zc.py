5419d56b0fd49d2506eeed39459f97e32c6482e1
farewell1990 <chun1990>
2014-08-14 13:16:07 +0800

    1

---
+++ framework/src/mychevy/test_stationAppointment_zc.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device
+from time import sleep
+#点击取消预约按钮，成功取消
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android
+
+    def tearDown(self):
+        #返回首页
+        device.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+        sleep(2)
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000004')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
+        #登录成功
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
+        sleep(3)
+        self.driver.find_element_by_name (u'详情').click()
+        el=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order')
+        el.click()
+        print el.text
+        print self.driver.current_activity
