f2130f13acca8dd509f288f2eaa41fa85a21c58a
farewell1990 <chun1990>
2014-08-14 14:09:40 +0800

    1

--- framework/src/mychevy/test_orderSucess_zc.py
+++ framework/src/mychevy/test_orderSucess_zc.py
@@ -3,7 +3,7 @@ __author__ = 'zhangchun'
 import unittest
 from framework.core import the,device
 from time import sleep
-#进入详情界面，点击预约保养按钮，进入预约界面
+#点击详情中的预约保养，选择指定时间，点击确定，预约成功
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = the.android
@@ -16,7 +16,6 @@ class TestCase(unittest.TestCase):
         #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
-        sleep(2)
         self.driver.switch_to_alert()
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000004')
@@ -24,14 +23,22 @@ class TestCase(unittest.TestCase):
         self.driver.switch_to_alert()
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
         #登录成功
+        sleep(3)
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
         sleep(3)
         self.driver.find_element_by_name (u'详情').click()
-        text1=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_name').text
-        el=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order')
-        el.click()
         sleep(3)
-        text2=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_location').text
-        print el.text
-        self.assertEqual('.OrderActivity',self.driver.current_activity)
-        self.assertEqual(text1,text2)
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order').click()
+        #进入预约保养界面
+        sleep(3)
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_order_time').click()
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
+        #选择期望时间
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order').click()
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
+        #预约成功后，保养预约按钮变为取消预约按钮
+        sleep(3)
+        text1=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_cancel').text
+        self.assertEqual(u'取消预约',text1)
