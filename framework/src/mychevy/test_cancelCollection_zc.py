08ea8b49cabf24ea7ece82088a00ad7707d681f5
farewell1990 <chun1990>
2014-08-14 11:06:24 +0800

    1

--- framework/src/mychevy/test_cancelCollection_zc.py
+++ framework/src/mychevy/test_cancelCollection_zc.py
@@ -3,7 +3,7 @@ __author__ = 'zhangchun'
 import unittest
 from framework.core import the,device
 from time import sleep
-#点击收藏按钮，在收藏的服务站中显示此服务站
+#点击取消收藏，服务站在收藏的服务站列表中消失
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver =the.android
@@ -15,17 +15,20 @@ class TestCase(unittest.TestCase):
     def test_case1(self):
         #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
+        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
+        # sleep(3)
+        # self.driver.find_element_by_name(u'详情').click()
+        # sleep(3)
+        # self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_favorite').click()
+        # # #点击收藏按钮
+        # device.switchToHome(self,self.mainActivity)
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
         sleep(3)
         self.driver.find_element_by_name(u'详情').click()
         sleep(3)
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_favorite').click()
-        #点击收藏按钮
-        device.switchToHome(self,self.mainActivity)
-        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
-        sleep(3)
+        self.driver.back()
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/rb_love_station').click()
-        #点击收藏的服务站
-        sleep(3)
-        el=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/ll_hour24_list_row')
-        self.assertTrue(el.is_enabled())#收藏的服务站列表中显示该服务站
+        txt=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_love_empty').text
+        self.assertFalse(u'上海强生北美汽车销售服务有限公司' in txt)
+
