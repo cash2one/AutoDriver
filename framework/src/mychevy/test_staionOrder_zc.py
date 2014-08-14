53bd93dc42b1348b83aed2c644e5c8697f13224c
farewell1990 <chun1990>
2014-08-14 13:13:51 +0800

    1

--- framework/src/mychevy/test_staionOrder_zc.py
+++ framework/src/mychevy/test_staionOrder_zc.py
@@ -6,14 +6,13 @@ from time import sleep
 #预约保养成功后，在服务站详情中显示取消预约按钮
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = device.android()
+        self.driver = the.android
 
     def tearDown(self):
         #返回首页
         device.switchToHome(self,self.mainActivity)
 
     def test_case1(self):
-        sleep(5)
         #每个测试用例，都需要把首页加入到变量mainActivity
         self.mainActivity = self.driver.current_activity
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
