4d7a7038034639304ad3572d1fc9f46ef3884670
farewell1990 <chun1990>
2014-08-15 16:10:30 +0800

    1

--- framework/src/mychevy/test_oldActivity_zc.py
+++ framework/src/mychevy/test_oldActivity_zc.py
@@ -3,7 +3,7 @@ __author__ = 'zhangchun'
 import unittest
 from framework.core import the,device
 from time import sleep
-#点击点赞按钮，上下滑动列表查看点赞数是否恢复原值
+#点击活动回顾，进入活动回顾页面
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = the.android
@@ -18,8 +18,11 @@ class TestCase(unittest.TestCase):
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_party').click()
         sleep(3)
         self.driver.find_element_by_name(u'聚乐会活动回顾').click()
+        text1=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/review_name').text
         self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/img_partyreview').click()
         sleep(3)
-        text1=self.driver.find_elements_by_class_name('android.widget.TextView')[0].text
-        self.assertEqual(u'聚乐会活动回顾',text1)
+        text2=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/details_name').text
+        text=self.driver.find_elements_by_class_name('android.widget.TextView')[0].text
+        self.assertEqual(u'聚乐会活动回顾',text)
+        self.assertEqual(text1,text2)
 
