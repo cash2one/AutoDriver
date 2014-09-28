3d9453081b055d5a56512960b1f887414c02903e
guohai@live.com <guohai@live.com>
2014-09-28 17:04:47 +0800

    testcase

--- framework/src/autobook/web/demo/find_str.py
+++ framework/src/autobook/web/demo/find_str.py
@@ -15,6 +15,12 @@ class TestCase(unittest.TestCase):
     def test_list1(self):
         self.ff.get('http://112.124.117.108/tupu/table/')
 
-        trs=self.ff.find_element_by_id('tableid').find_elements_by_tag_name('tr')
-        con = trs[0].find_elements_by_tag_name('th')[0].text
-        self.assertTrue('naa' in con,u'未发现指定字符')
+        table = self.ff.find_element_by_id('tableid')
+        tr = table.find_elements_by_tag_name('tr')
+
+        th0 = tr[0].find_elements_by_tag_name('th')[0]
+        txt = th0.text
+
+        self.assertTrue('n' in txt,'none')
+
+
