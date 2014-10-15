b91320ea9ce14d1339773644bbc68db3188af9ca
farewell1990 <chun1990>
2014-10-15 18:02:18 +0800

    Merge branch 'master' of https://github.com/haixy/TestCase
    Conflicts:
    	resource/config.ini

--- framework/src/autobook/android/driver/teat_driver_info_zc.py
+++ framework/src/autobook/android/driver/teat_driver_info_zc.py
@@ -25,25 +25,43 @@ class TestCase(unittest.TestCase):
         self.driver.find_id('personal_list_text').click()
         self.driver.switch_finish(current_activity)
         self.assertTrue('<bound method Android.current_activity of <framework.core.extend.Android object at 0x01FE1190>>',self.driver.current_activity)
+        #跳转到个人信息页面
 
-
-    def test_my_name(self):
-        self.my_info()
-        els=idriver.find_data('select name from t_driver where id =67')
-        name=self.driver.find_id('text_name').text
-        self.assertTrue(els,name)
+    # def test_my_name(self):
+    #     self.my_info()
+    #     els=idriver.find_data('select name from t_driver where no=140018')
+    #     name=self.driver.find_id('text_name').text
+    #     self.assertTrue(els,name)
+    #     #查看司机的姓名
 
     def test_my_country(self):
         self.my_info()
-        els=idriver.find_data('select driving_age from t_driver where id =67')
-        d_age=self.driver.find_id('text_driving_experience').text
-        self.assertTrue(els,d_age[-1])
+        t=idriver.find_data('select province from t_driver where no=140018')[0]
+        els=idriver.province(t)
+        d_age=self.driver.find_id('text_hometown').text
+        self.assertTrue(els,d_age[-2:])
+        print els
 
-    def test_my_count(self):
+    def test_my_license(self):
         self.my_info()
-        els=idriver.find_data('select driving_count from t_driver where id =67')
-        d_age=self.driver.find_id('text_service_times').text
-        self.assertTrue(els,d_age[-2:-1])
+        t=idriver.find_data('select license_type from t_driver where no=140018')[0]
+        els=idriver.license_type(t)
+
+        d_age=self.driver.find_id('text_driver_license').text
+        self.assertTrue(els,d_age[-2:])
+        print els
+
+    # def test_my_drivingAge(self):
+    #     self.my_info()
+    #     els=idriver.find_data('select driving_age from t_driver where no =140018')
+    #     d_age=self.driver.find_id('text_driving_experience').text
+    #     self.assertTrue(els,d_age[-1])
+    #
+    # def test_my_count(self):
+    #     self.my_info()
+    #     els=idriver.find_data('select driving_count from t_driver where no =140018')
+    #     d_age=self.driver.find_id('text_service_times').text
+    #     self.assertTrue(els,d_age[-2])
 
 
 
