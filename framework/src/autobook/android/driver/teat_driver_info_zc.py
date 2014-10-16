16a7547e5e9b0c2a34402aa63323ea8860fd89ff
farewell1990 <chun1990>
2014-10-16 13:24:12 +0800

    Merge branch 'master' of https://github.com/haixy/TestCase
    Conflicts:
    	resource/config.ini

--- framework/src/autobook/android/driver/teat_driver_info_zc.py
+++ framework/src/autobook/android/driver/teat_driver_info_zc.py
@@ -8,8 +8,8 @@ from framework.core import extend,idriver
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = extend.Android()
-        self.driver.login('idriver_driver')
+        self.driver = extend.Android('android.idriver_driver')
+        self.driver.login()
 
     def tearDown(self):
         #返回首页
@@ -29,14 +29,14 @@ class TestCase(unittest.TestCase):
 
     def test_my_name(self):
         self.my_info()
-        els=idriver.find_data('select name from t_driver where no=140018')
+        els=self.driver.sql('select name from t_driver where no=140018')
         name=self.driver.find_id('text_name').text
         self.assertTrue(els,name)
         #查看司机的姓名
 
     def test_my_country(self):
         self.my_info()
-        t=idriver.find_data('select province from t_driver where no=140018')[0]
+        t=self.driver.sql('select province from t_driver where no=140018')[0]
         els=idriver.province(t)
         d_age=self.driver.find_id('text_hometown').text
         self.assertTrue(els,d_age[-2:])
@@ -44,7 +44,7 @@ class TestCase(unittest.TestCase):
 
     def test_my_license(self):
         self.my_info()
-        t=idriver.find_data('select license_type from t_driver where no=140018')[0]
+        t=self.driver.sql('select license_type from t_driver where no=140018')[0]
         els=idriver.license_type(t)
 
         d_age=self.driver.find_id('text_driver_license').text
@@ -53,13 +53,13 @@ class TestCase(unittest.TestCase):
 
     def test_my_drivingAge(self):
         self.my_info()
-        els=idriver.find_data('select driving_age from t_driver where no =140018')
+        els=self.driver.sql('select driving_age from t_driver where no =140018')
         d_age=self.driver.find_id('text_driving_experience').text
         self.assertTrue(els,d_age[-1])
 
     def test_my_count(self):
         self.my_info()
-        els=idriver.find_data('select driving_count from t_driver where no =140018')
+        els=self.driver.sql('select driving_count from t_driver where no =140018')
         d_age=self.driver.find_id('text_service_times').text
         self.assertTrue(els,d_age[-2])
 
