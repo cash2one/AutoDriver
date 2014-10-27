cbd7d9b0902527fa4d5064899a3004338714ea0b
guohai@live.com <guohai@live.com>
2014-10-27 10:43:55 +0800

    device

--- framework/src/autobook/web/cs/test_demo.py
+++ framework/src/autobook/web/cs/test_demo.py
@@ -7,20 +7,24 @@ from framework.core import device
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.firefox = device.app('idriver.web.cs', device.firefox)
+        self.ff = device.app('idriver.web.cs', device.firefox)
 
     def tearDown(self):
         pass
 
     def test_list1(self):
-        self.firefox.get('http://www.w3school.com.cn/')
-        txt = self.firefox.find_elements_by_id('h').text
+        self.ff.get('http://www.w3school.com.cn/')
+        txt = self.ff.find_elements_by_id('h').text
 
         self.assertTrue(u'HTML 系列教程' in txt ,'false')
 
 
     def test_list2(self):
-        self.firefox.get('http://www.w3school.com.cn/')
-        txt = self.firefox.find_id('w3').find_tag('h2').text
+        self.ff.get('http://www.w3school.com.cn/')
+        #txt = self.ff.find_id('w3').find_tag('h2').text
+        print self.ff.find_id('w3')
+        print self.ff.find_tag('h2')
+        txt = self.ff.find_element_by_id('w3').find_element_by_tag_name('h2').text
+
 
         self.assertTrue(u'领先的 Web 技术教程 - 全部免费' in txt , 'false')
