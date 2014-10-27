aeef0520ca73bc76598efa5bc2c1c055dc12735b
guohai@live.com <guohai@live.com>
2014-10-27 18:44:14 +0800

    device

--- framework/src/autobook/android/driver/test_myclass.py
+++ framework/src/autobook/android/driver/test_myclass.py
@@ -10,6 +10,7 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = device.app('idriver.android.driver')
         idriver.login_driver(self.driver)
+        self.pkg = self.driver.package
 
     def tearDown(self):
         #返回首页
@@ -22,7 +23,7 @@ class TestCase(unittest.TestCase):
         # name_id = self.driver.wait_id_text('tv_customer_name',u'xu女士')
         #
         # print 'pass.....'
-
+        print self.pkg
         txt = self.driver.find_id('ll_order_body').find_id('tv_order_id').text
         print txt
 
