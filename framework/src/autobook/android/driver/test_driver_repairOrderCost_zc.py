3bab77f3b1dc8f50a0f3f7bb261cdaf7b52d9f48
farewell1990 <chun1990>
2014-10-28 17:18:03 +0800

    Merge branch 'master' of https://github.com/haixy/TestCase
    Conflicts:
    	resource/config.ini

---
+++ framework/src/autobook/android/driver/test_driver_repairOrderCost_zc.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+
+import time
+from framework.core import device,idriver
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = device.app('idriver.android.driver')
+        idriver.login_driver(self.driver)
+        self.driver_no = idriver.get_driver_no()
+        #获取登录司机的工号
+
+    def tearDown(self):
+        #返回首页
+        self.driver.switch_to_home()
+
+    def test_month_earning(self):
+        idriver.changeWork(self.driver,True)
+        current_activity = self.driver.current_activity
+        #获取待补订单列表中订单的信息
+        self.driver.find_id('iv_detail').click()
+        self.driver.wait_switch(current_activity)
+
+        self.driver.find_id('ro_endtime').click()
+        self.driver.switch_to_alert()
+        self.driver.find_elements_by_class_name('android.widget.ImageButton')[6].click()
+        self.driver.find_id('btn_ok').click()
+        time.sleep(3)
+        self.driver.find_id('ro_eaddr').send_keys('ggfdg')
+        time.sleep(3)
+        self.driver.find_id('confirm_repairorder').click()
+        self.driver.switch_to_alert()
+
+        txt=self.driver.find_id('tv_msg').text
+        self.assertTrue(u'费用合计不能低于39元' in txt)
+
