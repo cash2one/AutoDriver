c46654191043dd951bf34a5b8ed5d4def6f4866f
farewell1990 <chun1990>
2014-10-30 10:34:04 +0800

    Merge branch 'master' of https://github.com/haixy/TestCase
    Conflicts:
    	resource/config.ini

--- framework/src/autobook/android/driver/test_driver_repairOrder1_zc.py
+++ framework/src/autobook/android/driver/test_driver_repairOrder1_zc.py
@@ -19,6 +19,32 @@ class TestCase(unittest.TestCase):
 
     def test_history_order(self):
         idriver.changeWork(self.driver,True)
+        current_activity = self.driver.current_activity
+        self.driver.find_id('iv_head').click()
+        self.driver.wait_switch(current_activity)
+
+        current_activity = self.driver.current_activity
+        self.driver.find_element_by_name(u'历史订单').click()
+        self.driver.wait_switch(current_activity)
+
+        text1=self.driver.find_id('order_number_text').text
+        order_no=text1.split(':')[1]
+        #获取最近一个订单的订单号
+
+        # text2=self.driver.find_id('order_amount_text').text
+        # t=text2.split(':')[1]
+        # order_amount=filter(str.isdigit,str(t[:-1]))
+        # #获取最近一个订单的费用合计
+        #
+        # text3=self.driver.find_id('order_time_text').text
+        # order_time=text3.split(':')[1]
+        # #获取最近一个订单的时间
+        #
+        # text4=self.driver.find_id('completed_saddr').text
+        # order_time=text3.split(':')[1]
+        # #获取最近一个订单的时间
 
 
-
+        orders_no=self.driver.sql('SELECT a.order_no FROM t_order_info a, t_driver b WHERE a.driver_id = b.id and b.no =140013',1)
+        #self.assertTrue(order_no in orders_no)
+        print(orders_no)
