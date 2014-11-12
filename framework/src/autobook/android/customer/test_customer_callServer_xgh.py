3cd2d443216f44c99dccb9be6fb97b44e34422d7
xuguanghua <guanghua_2011@126.com>
2014-11-12 18:13:15 +0800

    ghuu

---
+++ framework/src/autobook/android/customer/test_customer_callServer_xgh.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+__author__ = 'xuguanghua'
+
+import time
+import unittest
+from framework.core import idriver_android
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = idriver_android.customer()
+        #self.driver.login()
+
+    def tearDown(self):
+        self.driver.switch_to_home()
+
+    def test_call_server(self):
+        current_activity = self.driver.current_activity
+        #在附近司机界面点击联系客服按钮
+        self.driver.find_id('btn_callserver').click()
+        self.driver.switch_to_alert()
+        text_msg = self.driver.find_id('tvv_msg').text
+        self.assertTrue(u'将拨打客服电话400-800-8888' in text_msg ,'msg')
+        #点击拨打按钮
+        self.driver.find_id('btn_right').click()
