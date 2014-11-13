d6f6bb917dfb4353b978ceb6286faf805533f312
farewell1990 <chun1990>
2014-11-13 15:15:28 +0800

    Merge branch 'master' of https://github.com/haixy/TestCase
    Conflicts:
    	resource/config.ini

--- framework/src/autobook/android/driver/test_driver_inform_zc.py
+++ framework/src/autobook/android/driver/test_driver_inform_zc.py
@@ -21,9 +21,9 @@ class TestCase(unittest.TestCase):
         current_activity = self.driver.current_activity
         try:
             el=self.driver.find_id('tv_msg_count')
+            #若有未读标志，则走if流程
             if el.is_displayed():
                 text1=el.text
-
                 self.driver.find_id("iv_head").click()
                 self.driver.wait_switch(current_activity)
                 text2=self.driver.find_id("personal_msg_count").text
@@ -32,11 +32,9 @@ class TestCase(unittest.TestCase):
                 red=self.driver.find_ids('bulletin_read')
                 text3=len(red)
                 self.assertTrue(text1==text2==unicode(text3))
-
+                #未读条数与红色标记的数目一致
             else:
                 pass
-
-
         except :
             self.driver.find_id("iv_head").click()
             self.driver.wait_switch(current_activity)
@@ -44,3 +42,4 @@ class TestCase(unittest.TestCase):
             txt=self.driver.find_elements_by_class_name('android.widget.TextView')[1].text
             self.assertTrue(u"当前没有消息提示" in txt)
             print("no informs")
+            #若没有未读标志，公告列表页面则显示当前没有消息提示
