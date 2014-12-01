bbd04f1df503155ebb9d2404468d666802967969
guohai <guohai@live.com>
2014-12-01 11:17:28 +0800

    order robot

--- framework/src/autobook/android/customer/test_order.py
+++ framework/src/autobook/android/customer/test_order.py
@@ -13,8 +13,6 @@ class TestCase(unittest.TestCase):
         pass
 
     def test_call_server(self):
-
-        PATH = lambda p: os.path.abspath(
-            os.path.join(os.path.dirname(__file__), p)
-        )
-        print self.driver.auto_order(PATH('./test_order_robot.py'))
+        script = os.path.join(os.path.dirname(__file__), './test_order_robot.py')
+        print script
+        print self.driver.auto_order(script)
