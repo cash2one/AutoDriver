32f279ccffe7d4adbd575f537dfb2096b2c2a664
gaoxu <gxsmile0809@163.com>
2014-12-03 17:16:47 +0800

    a

--- framework/src/autobook/android/driver/test_driver_login_gx.py
+++ framework/src/autobook/android/driver/test_driver_login_gx.py
@@ -12,6 +12,7 @@ class TestCase(unittest.TestCase):
         self.driver = idriver_android.driver()
         # self.driver.login()
 
+
     def tearDown(self):
         #返回首页
         self.driver.switch_to_home()
