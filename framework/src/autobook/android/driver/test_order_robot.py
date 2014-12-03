da33b8cf69bb20820ff91d132add6618df37b04b
farewell1990 <chun1990>
2014-12-03 17:13:21 +0800

    Merge branch 'master' of https://github.com/haixy/TestCase
    Conflicts:
    	resource/config.ini

--- framework/src/autobook/android/driver/test_order_robot.py
+++ framework/src/autobook/android/driver/test_order_robot.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-__author__ = 'guanghua_2011@126.com'
+__author__ = 'zhangchun@pathbook.com.cn'
 
 import time
 import unittest
@@ -8,14 +8,17 @@ from framework.core import idriver_android
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.driver = idriver_android.customer_robot()
-        pass
+        self.driver.login()
 
     def tearDown(self):
         pass
 
     def test_call_order(self):
         #写具体执行下订单操作
-        print u'我已经下了订单'
+        current_activity = self.driver.current_activity
+        self.driver.find_id('bt_order').click()
+
+
 
 if __name__ == '__main__':
     unittest.main()
