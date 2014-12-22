a1a7518ed44d2334b336b2d4d3137d1a2b740951
farewell1990 <chun1990>
2014-12-22 11:45:06 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	resource/app/Customer.apk

---
+++ testcase/MyChevy/test_stationAppointment_zc.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device_bak
+from time import sleep
+#进入详情界面，点击预约保养按钮，进入预约界面
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android
+
+    def tearDown(self):
+        #返回首页
+        device_bak.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/iv_user_icon').click()
+        sleep(2)
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_name').send_keys('gsd')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_phone').send_keys('13800000004')
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/user_ok').click()
+        self.driver.switch_to_alert()
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_ok').click()
+        #登录成功
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
+        sleep(3)
+        self.driver.find_element_by_name (u'详情').click()
+        text1=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_name').text
+        el=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_order')
+        el.click()
+        sleep(3)
+        text2=self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/tv_location').text
+        print el.text
+        self.assertEqual('.OrderActivity',self.driver.current_activity)
+        self.assertEqual(text1,text2)
---
+++ testcase/MyChevy/test_stationDetails_zc.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+__author__ = 'zhangchun'
+import unittest
+from framework.core import the,device_bak
+from time import sleep
+#点击收藏按钮，在预约保养的收藏的服务站中显示此服务站
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = the.android
+
+    def tearDown(self):
+        #返回首页
+        device_bak.switchToHome(self,self.mainActivity)
+
+    def test_case1(self):
+        #每个测试用例，都需要把首页加入到变量mainActivity
+        self.mainActivity = self.driver.current_activity
+        sleep(3)
+        self.driver.find_element_by_id('cn.com.pathbook.mychevy:id/btn_station').click()
+        sleep(3)
+        self.driver.find_element_by_name(u'详情').click()
+        sleep(3)
+        #print(self.driver.current_activity)
+        self.assertEqual('.StationDetailActivity',self.driver.current_activity)
