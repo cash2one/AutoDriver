e41346117eca7d581142790ef913348e7ba72485
farewell1990 <chun1990>
2014-12-29 17:15:53 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	resource/app/Customer.apk

--- testcase/Autobook/op/test_op_addRole.py
+++ testcase/Autobook/op/test_op_addRole.py
@@ -1,5 +1,6 @@
-__author__ = 'zhangchun@pathbook.com.cn'
 # coding=utf-8
+__author__ = 'zhangchun@pathbook.com.cn'
+
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -45,13 +46,12 @@ class TestCase(unittest.TestCase):
         self.driver.find_id('role_name').send_keys(u'财务超级管理9')
         self.driver.find_id('sure_create_role_btn').click()
         self.driver.switch_to_alert()
-
         time.sleep(2)
         text=self.driver.find_element_by_class_name('xubox_main').text
         self.assertTrue(u"添加角色成功" in text)
         self.driver.find_element_by_xpath(u'/html/body/div[5]/div[1]/span[2]/a').click()
         self.driver.wait_find_id('')
-        name=self.driver.find_id('list').find_elements_by_class_name('tr')[1].find_elements_by_class_name('td')[2].text
+        name=self.driver.find_id('list').find_elements_by_class_name('tr')[0].find_elements_by_class_name('td')[2].text
         self.assertTrue(name==u'财务超级管理9')
 
 
