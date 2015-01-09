6a3e5c69d4f889453abbe262e6e54009ba882673
farewell1990 <chun1990>
2015-01-09 16:43:16 +0800

    add autobook op

--- testcase/Autobook/op/test_op_querySysInfo.py
+++ testcase/Autobook/op/test_op_querySysInfo.py
@@ -21,10 +21,24 @@ class TestCase(unittest.TestCase):
 
 
     def test_email1(self):
-        above=self.driver.find_element_by_xpath('/html/body/div[2]/ul/li[4]/a')
+        above=self.driver.find_element_by_link_text(u'系统管理')
 
         ActionChains(self.driver).move_to_element(above).perform()
         #鼠标悬停在系统管理上
         self.driver.find_element_by_link_text(u'系统参数').click()
+        opts=self.driver.find_id('paramType').find_elements_by_tag_name('option')
 
+        for opt in opts:
+            if opt.get_attribute('text')==u'接口配置':
+                opt.click()
+        self.driver.find_id('query').click()
+        trs=self.driver.find_id('list').find_elements_by_tag_name('tr')
+        #查询条件状态选择接口配置
+        if len(trs)>1:
+            for i in range(1,len(trs)-1):
+                text=trs[i].find_elements_by_tag_name('td')[1].text
+                self.assertEqual(text,u"接口配置")
+        else:
+            text=self.driver.find_element_by_class_name('norecords').text
+            self.assertTrue(u'没有符合条件的数据'in text)
 
