f69604801c9f89ba6a26603b629cf2ab698df611
guohai <guohai@live.com>
2015-01-12 18:25:18 +0800

    gui read api to treeview

--- framework/gui/api_test.py
+++ framework/gui/api_test.py
@@ -7,8 +7,11 @@ from PyQt4 import QtCore
 from framework.gui.views import interface_ui
 from framework.gui.models import listview_model
 from framework.gui.models import tree_model_apis,tree_model
-from framework.gui.helpers import param_infr
+from framework.util import fs
 
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
 
 class InterfaceForm(QWidget, interface_ui.Ui_Form):
     def __init__(self):
@@ -22,10 +25,19 @@ class InterfaceForm(QWidget, interface_ui.Ui_Form):
 
         #tree_model = listview_model.StampTreeModel(param_infr.inf)  # param_infr.inf, self
         #self.tree_infs.clicked.connect(self.itemSelected)
+        inf = []
+        dirs=PATH('../../resource/api/')
+        xml_files=os.listdir(dirs)
+        for x in xml_files:
+            dicts={}
+            dicts['name']=''
+            dicts['api'] = fs.read_xml(os.path.join(dirs,x))
+            inf.append(dicts)
+        print inf
 
         f = QtCore.QFile(':/default.txt')
         f.open(QtCore.QIODevice.ReadOnly)
-        model = tree_model_apis.TreeModel(param_infr.inf) #param_infr.inf
+        model = tree_model_apis.TreeModel(inf) #param_infr.inf
         self.tree_infs.setModel(model)
 
 
