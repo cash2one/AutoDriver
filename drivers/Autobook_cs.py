5372e49f8853ad481ef20a45fb836fc057c254ba
uuoai <guohai@live.com>
2015-01-15 01:17:51 +0800

    autobook_cs extend firefox and chrome

--- drivers/Autobook_cs.py
+++ drivers/Autobook_cs.py
@@ -12,11 +12,25 @@ def Application(config):
         return Chrome(config)
 
 
-class Firefox(web.Firefox):
+class MyDriver(object):
+    '''
+    firefox chrome 都通用此类
+    '''
+
+    def __init__(self):
+        pass
+
+    def method1(self):
+        return 'a'
+
+
+class Firefox(web.Firefox, MyDriver):
     def __init__(self, config):
         super(Firefox, self).__init__(config)
 
 
-class Chrome(web.Chrome):
+class Chrome(web.Chrome, MyDriver):
     def __init__(self, config):
         super(Chrome, self).__init__(config)
+
+
