5a9c7aca1577c1cad3207e967c39cc1e04ffa7bb
farewell1990 <chun1990>
2015-01-16 15:36:58 +0800

    add autobook op

--- testcase/Autobook/op/test_op_dateOrder.py
+++ testcase/Autobook/op/test_op_dateOrder.py
@@ -19,9 +19,9 @@ class TestCase(unittest.TestCase):
         #返回首页
         self.driver.switch_to_home()
 
-    def test_dateControl(self):
+    def test_datelist(self):
         '''
-        结束时间小于开始时间，系统给出错误提示
+        选择开始时间及结束时间，提交后，列表中显示该时间范围内的订单记录
         :return:
         '''
         above=self.driver.find_element_by_link_text(u'统计查询')
@@ -33,22 +33,22 @@ class TestCase(unittest.TestCase):
         js = '$(\'input[id=startTime_amount]\').removeAttr(\'readonly\')'
         self.driver.execute_script(js)
         self.driver.find_element_by_id('startTime_amount').clear()
-        self.driver.find_element_by_id('startTime_amount').send_keys('2015-01-06')
+        time1='2015-01-06'
+        self.driver.find_element_by_id('startTime_amount').send_keys(time1)
         #选择开始时间
         js = '$(\'input[id=endTime_amount]\').removeAttr(\'readonly\')'
         self.driver.execute_script(js)
         self.driver.find_element_by_id('endTime_amount').clear()
-        self.driver.find_element_by_id('endTime_amount').send_keys('2015-01-01')
+        time2='2015-01-15'
+        self.driver.find_element_by_id('endTime_amount').send_keys(time2)
         self.driver.find_id('statistics_amount').click()
         #选择结束时间
         time.sleep(3)
-        text=self.driver.switch_to_alert().text
-        self.assertEqual(text,u'开始日期不能大于结束时间，请重新选择！')
-        self.driver.switch_to_alert().accept()
-        print(text)
-
-
-
-
-
+        trs=self.driver.find_element_by_id('list').find_elements_by_tag_name('tr')
+        start=trs[1].find_elements_by_tag_name('td')[0].text
+        startTime=start[0:4]+'-'+start[5:7]+'-'+start[-3:-1]
+        end=trs[len(trs)-1].find_elements_by_tag_name('td')[0].text
+        endTime=end[0:4]+'-'+end[5:7]+'-'+end[-3:-1]
+        self.assertEqual(time1,startTime)
+        self.assertEqual(time2,endTime)
 
