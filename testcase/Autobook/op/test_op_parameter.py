6f850166b4fc15dabe20d037146f4de7347cbe6b
xhl <xhl13918486416@163.com>
2015-01-16 16:33:56 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	framework/src/emanual/web/ir/test_lianxi_xhl.py

--- testcase/Autobook/op/test_op_parameter.py
+++ testcase/Autobook/op/test_op_parameter.py
@@ -8,12 +8,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.action_chains import ActionChains
-from framework.core import idriver_web
+from framework.core import testcase
 import os
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = idriver_web.firefox(__file__)
+        self.driver = testcase.app(__file__)
         self.driver.login()
 
     def tearDown(self):
@@ -25,8 +25,10 @@ class TestCase(unittest.TestCase):
         ActionChains(self.driver).move_to_element(above).perform()
         #鼠标悬停在日志查询上
         self.driver.find_element_by_link_text(u'接口访问日志').click()
+        #在请求参数文本框里面输入aaaaa
         self.driver.find_element_by_id('params').send_keys(u'aaaaa')
         self.driver.find_element_by_id('query').click()
+        time.sleep(3)
         trs=self.driver.find_element_by_id('list').find_elements_by_tag_name('tr')
         print len(trs)
         #判断长度是不是小于1
@@ -35,12 +37,57 @@ class TestCase(unittest.TestCase):
             for i in range(1,len(trs)-1):
                 #找到td
                 text=trs[i].find_elements_by_tag_name('td')[6].text
-                #判断是不是td里面的接口名称是不是driverService.dealOrder
+                #判断是不是td里面的  请求参数是不是aaaaa
+                self.assertTrue(u'aaaaa'in text)
+        else:
+            text=self.driver.find_element_by_class_name('norecords').text
+            self.assertTrue(u'没有符合条件的数据'in text)
+
+    def test_logReques(self):
+        above=self.driver.find_element_by_link_text(u'日志查询')
+        ActionChains(self.driver).move_to_element(above).perform()
+        #鼠标悬停在日志查询上
+        self.driver.find_element_by_link_text(u'接口访问日志').click()
+        #在请求参数文本框里面输入aaaaa
+        self.driver.find_element_by_id('requestIdSearch').send_keys('14017814213006828048340')
+        self.driver.find_element_by_id('query').click()
+        time.sleep(3)
+        trs=self.driver.find_element_by_id('list').find_elements_by_tag_name('tr')
+
+        #判断长度是不是小于1
+        if len(trs)>1:
+            #循环行
+            for i in range(1,len(trs)-1):
+                #找到td
+                text=trs[i].find_elements_by_tag_name('td')[8].text
+                #判断是不是td里面的  请求参数是不是14017814213006828048340
                 print text,i
-                tuple=u'cancelType=1&orderNo=15011315092036&memo=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&requestId=14000513261766449233646&event=7&tokenNo=140005F4EFFC4E8A1D40FE850D85181D1CBC14'
-                self.assertTure('aaaaa' in text)
+                self.assertTrue('14017814213006828048340' in text)
         else:
             text=self.driver.find_element_by_class_name('norecords').text
             self.assertTrue(u'没有符合条件的数据'in text)
 
 
+    def test_logException(self):
+        above=self.driver.find_element_by_link_text(u'日志查询')
+        ActionChains(self.driver).move_to_element(above).perform()
+        #鼠标悬停在日志查询上
+        self.driver.find_element_by_link_text(u'接口访问日志').click()
+        #在请求参数文本框里面输入aaaaa
+        self.driver.find_element_by_id('content').send_keys(u'{"res":"-2031","msg":"令牌失效"}')
+        self.driver.find_element_by_id('query').click()
+        time.sleep(3)
+        trs=self.driver.find_element_by_id('list').find_elements_by_tag_name('tr')
+
+        #判断长度是不是小于1
+        if len(trs)>1:
+            #循环行
+            for i in range(1,len(trs)-1):
+                #找到td
+                text=trs[i].find_elements_by_tag_name('td')[9].text
+                #判断是不是td里面的  请求参数是不是res":"-2031","msg":"令牌失效
+                print text,i
+                self.assertTrue(u'{"res":"-2031","msg":"令牌失效"}' in text)
+        else:
+            text=self.driver.find_element_by_class_name('norecords').text
+            self.assertTrue(u'没有符合条件的数据'in text)
