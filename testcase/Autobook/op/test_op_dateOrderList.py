cdaac4e03be1792a74dd1e797543fe1541f3b7f8
farewell1990 <chun1990>
2015-01-16 18:03:41 +0800

    add autobook op

--- testcase/Autobook/op/test_op_dateOrderList.py
+++ testcase/Autobook/op/test_op_dateOrderList.py
@@ -19,7 +19,35 @@ class TestCase(unittest.TestCase):
         #返回首页
         self.driver.switch_to_home()
 
+
+
     def test_datelist(self):
+        '''
+        不选择开始时间及结束时间，提交后，列表中默认显示本月1号到前一天的订单记录
+        :return:
+        '''
+        above=self.driver.find_element_by_link_text(u'统计查询')
+
+        ActionChains(self.driver).move_to_element(above).perform()
+        #鼠标悬停在统计查询
+        self.driver.find_element_by_link_text(u'订单统计').click()
+        year=datetime.date.today().year
+        month=datetime.date.today().month
+        startTime1=datetime.date(year,month,01)
+        endTime1=datetime.date.today()-datetime.timedelta(days=1)
+        self.driver.find_id('statistics_amount').click()
+        #选择结束时间
+        time.sleep(3)
+        trs=self.driver.find_element_by_id('list').find_elements_by_tag_name('tr')
+        start=trs[1].find_elements_by_tag_name('td')[0].text
+        startTime=start[0:4]+'-'+start[5:7]+'-'+start[-3:-1]
+        end=trs[len(trs)-1].find_elements_by_tag_name('td')[0].text
+        endTime=end[0:4]+'-'+end[5:7]+'-'+end[-3:-1]
+        print startTime1,endTime1
+        self.assertEqual(str(startTime1),startTime)
+        self.assertEqual(str(endTime1),endTime)
+
+    def test_datelist1(self):
         '''
         选择开始时间及结束时间，提交后，列表中显示该时间范围内的订单记录
         :return:
