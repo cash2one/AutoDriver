935744a75c6fdb68fdc000668da42ac35417290e
guohai <guohai@live.com>
2015-02-06 11:58:14 +0800

    ios

--- woodpecker/devices.py
+++ woodpecker/devices.py
@@ -19,7 +19,7 @@ PATH = lambda p: os.path.abspath(
 
 
 class RecordingForm(QWidget, recording_ui.Ui_Form):
-    def __init__(self,parent_size):
+    def __init__(self, parent_size):
         super(RecordingForm, self).__init__()
 
         self.setupUi(self)
@@ -34,12 +34,10 @@ class RecordingForm(QWidget, recording_ui.Ui_Form):
         self.img_label = label_btn.LabelButton()
         self.img_label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
 
-        self.con_heihgt = self.height() - self.txt_elements.height() - 18
-        print self.con_heihgt,'self.height():',self.height(),'txt_elements:',self.txt_elements.height(),'init----'
-        self.img_label.setMinimumHeight(self.con_heihgt)
-        self.img_label.setMaximumHeight(self.con_heihgt)
+        self.con_height = parent_size - 320  # toolbar+输出框+内边距
+        self.img_label.setMinimumHeight(self.con_height)
+        self.img_label.setMaximumHeight(self.con_height)
         self.img_layout.addWidget(self.img_label)
-        print self.parent()
 
 
     def connect_device(self):
@@ -58,11 +56,9 @@ class RecordingForm(QWidget, recording_ui.Ui_Form):
 
     def screenshot(self):
         self.connect(self.img_label, SIGNAL("my_click"), self.handler_devices)
-
         self.img.screen_shot()
 
         png_dir = PATH('../temporary/')
-
         # 清除原先加载的Label
         # for i in reversed(range(0, self.img_layout.count())):
         # self.img_layout.itemAt(i).widget().deleteLater()
@@ -71,7 +67,7 @@ class RecordingForm(QWidget, recording_ui.Ui_Form):
         self.origin_png = QPixmap()
         self.origin_png.load(os.path.join(png_dir, 'temp.png'))
 
-        self.png = self.resize_image(self.origin_png, self.con_heihgt)
+        self.png = self.resize_image(self.origin_png, self.con_height)
         self.img_label.setPixmap(self.png)
         return self.png.width(), self.png.height()
         # self.img_label.setMinimumWidth(self.png.width())
@@ -89,16 +85,14 @@ class RecordingForm(QWidget, recording_ui.Ui_Form):
         # self.txt_elements.setText(txt_eles)
 
     def resize_image(self, png, height_):
-        print height_,'ffffffffff'
         picSize = QSize(height_, height_)
         p_img = png.scaled(picSize, Qt.KeepAspectRatio)
         return p_img
 
     def handler_devices(self, event):
         ss = self.scale_size
-        print event.x(), ss[0]
         self.action.touchByPos(event.x() * ss[0], event.y() * ss[1])
-        print self.img_label.height(),self.height(),self.txt_elements.height()
+        print self.img_label.height(), self.height(), self.txt_elements.height()
 
     def action_home(self):
         self.action.sendKeyEvent(keycode.HOME)
