7fc7014120f601cca6e67a5af5e0443770e8c12b
ggh <yxhgghG6>
2015-04-14 18:24:41 +0800

    简易操作界面

--- framework/gui/gui.py
+++ framework/gui/gui.py
@@ -13,54 +13,148 @@ class MainWin():
     def __init__(self, case_data):
         self.root = Tk()
         self.root.title("AutoDriver")
-        self.set_window_center(900, 600)
+        self.set_window_center(self.root, 570, 350)
         self.case_data = case_data
         self.current_folder = ''
 
+        menubar = Menu(self.root)
+
+        file_menu = Menu(menubar, tearoff=0)
+        file_menu.add_command(label='新建测试任务', command=self.hello)
+        file_menu.add_command(label='退出', command=self.hello)
+        menubar.add_cascade(label='文件', menu=file_menu)
+
+        history_menu = Menu(menubar, tearoff=0)
+        history_menu.add_command(label='任务1', command=self.hello)
+        history_menu.add_command(label='任务2', command=self.hello)
+        # 所有结果列表都做为子菜单
+        menubar.add_cascade(label='历史任务', menu=history_menu)
+        self.root.config(menu=menubar)
+
+        report_menu = Menu(menubar, tearoff=0)
+        report_menu.add_command(label='发送', command=self.hello)
+        report_menu.add_command(label='输出报告', command=self.hello)
+        menubar.add_cascade(label='发送报告', menu=report_menu)
+
+    def hello(self):
+        print 'hello'
 
     def base_form(self, master):
-        frame = Frame(master,pady=15)
+        frame_form = Frame(master)
+        label_task = Label(frame_form, text='任务名称：')
+        label_task.grid(row=0, column=0, sticky='w', padx=0, pady=5)
+        e = StringVar()
+        e.set('input your text here')
+        ipt_task = Entry(frame_form, text='0', width=55, textvariable=e)
+        ipt_task.grid(row=0, column=1, sticky='w', columnspan=3, padx=0, pady=5)
 
-        label_task = Label(frame, text='任务名称')
-        label_task.grid(row=0, column=0, sticky='ns',pady=5)
-        ipt_task = Entry(frame, text='0',width=50)
-        ipt_task.grid(row=0, column=1, sticky='ew',pady=5,columnspan=3)
+        label_cases = Label(frame_form, text='选择用例：')
+        label_cases.grid(row=1, column=0, sticky='w', padx=0, pady=5)
+        ipt_cases = Entry(frame_form, text='0', width=55)
+        ipt_cases.grid(row=1, column=1, sticky='w', pady=5, columnspan=3)
+        btn_cases = Button(frame_form, text='选择', command=lambda: self.func())
+        btn_cases.grid(row=1, column=4, sticky='w', pady=5)
 
-        label_name = Label(frame, text='用例执行次数')
-        label_name.grid(row=1, column=0, sticky='ns',pady=5)
-        ipt_name = Entry(frame, text='0')
-        ipt_name.grid(row=1, column=1, sticky='ew',pady=5)
+        label_name = Label(frame_form, text='执行次数：')
+        label_name.grid(row=2, column=0, sticky='w', padx=0, pady=5)
+        ipt_name = Entry(frame_form, text='0')
+        ipt_name.grid(row=2, column=1, sticky='w', padx=0, pady=5)
 
-        label_creator = Label(frame, text='执行人：')
-        label_creator.grid(row=1, column=2, sticky='ns',pady=5)
-        ipt_creator = Entry(frame, text='0')
-        ipt_creator.grid(row=1, column=3, sticky='ew',pady=5)
+        label_creator = Label(frame_form, text='执行人：')
+        label_creator.grid(row=2, column=2, sticky='w', padx=0, pady=5)
+        ipt_creator = Entry(frame_form, text='0')
+        ipt_creator.grid(row=2, column=3, sticky='w', padx=0, pady=5)
 
-        label_cases = Label(frame, text='选择用例：')
-        label_cases.grid(row=2, column=0, sticky='ns',pady=5)
-        ipt_cases = Entry(frame, text='0')
-        ipt_cases.grid(row=2, column=1, sticky='ew',pady=5)
-        btn_cases = Button(frame, text='选择')
-        btn_cases.grid(row=2, column=2, sticky='ns',pady=5)
+        # 按钮
+        # frame_action = Frame(master)
+        btn_start = Button(frame_form, text='执行用例', command=lambda: self.func())
+        btn_start.grid(row=3, column=1, sticky='w', pady=5)
+        btn_stop = Button(frame_form, text='停止用例')
+        btn_stop.grid(row=3, column=2, sticky='w', pady=5)
+        btn_report = Button(frame_form, text='输出报告')
+        btn_report.grid(row=3, column=3, sticky='w', pady=5)
 
-        btn_start = Button(frame, text='执行')
-        btn_start.grid(row=3, column=0, sticky='ns',pady=5)
-        btn_stop = Button(frame, text='停止')
-        btn_stop.grid(row=3, column=1, sticky='ns',pady=5)
-        btn_report = Button(frame, text='报告')
-        btn_report.grid(row=3, column=2, sticky='ns',pady=5)
+        frame_form.pack(fill=BOTH, expand=1, padx=20, pady=10)
+
+        scrollbar = Scrollbar(master, orient=VERTICAL)
+        listbox_status = Listbox(master, height=9, yscrollcommand=scrollbar.set, borderwidth=0)
+        listbox_status.pack(side=LEFT, fill=BOTH, expand=1)
+        scrollbar.config(command=self.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        # frame_list.pack(fill=BOTH, expand=1, padx=20, pady=0)
+
+        # listbox_status = Listbox(frame_action, width=78, height=9, yscrollcommand=scrollbar.set)
+        # scrollbar.configure(command=listbox_status.yview)
+        # listbox_status.grid(row=2, column=0, sticky='wn', columnspan=4, padx=0, pady=5)
+
+        listbox_status.insert(END, '测试结果信息')
+
+    def func(self):
+        self.top = Toplevel(self.root)
+        self.set_window_center(self.top, 500, 400)
+
+        frame1 = Frame(self.top, width=100)
+
+        tree = ttk.Treeview(frame1)
+        ysb = ttk.Scrollbar(frame1, orient='vertical', command=tree.yview)
+        tree.configure(yscroll=ysb.set)
+        tree.heading('#0', text='Path', anchor='w')
+
+        root_node = tree.insert('', 'end', text='TestCase', open=True)
+        self.process_directory(tree, root_node, PATH('../../testcase/'))
+
+        tree.grid(row=0)
+        ysb.grid(row=0, column=1, sticky=NS)  # pack(side=RIGHT, fill=Y)
+        frame1.grid(row=0, column=0)
+
+        frame2 = Frame(self.top, width=100)
+        cnames = StringVar()
+        # cnames.set(tuple(tnames))
+        listbox = Listbox(frame2, listvariable=cnames, selectmode='extended')
+        listbox.grid(row=0, column=0)
+
+        frame2.grid(row=0, column=1)
+
+        btn_select = Button(self.top, text='选择', command=lambda: self.select_node(tree, listbox))
+        btn_select.grid(row=1, column=0)
+        btn_del = Button(self.top, text='不选', command=lambda: self.del_box_item(listbox))
+        btn_del.grid(row=1, column=1)
+        btn_ok = Button(self.top, text='确定', command=lambda: self.select_ok(listbox))
+        btn_ok.grid(row=1, column=2)
 
 
-        label_status = Label(frame, text='用例执行状态：')
-        label_status.grid(row=4, column=0, sticky='ns',pady=5)
+    def select_node(self, tree, box):
+        # print tree.selection()
+        sel_tup = tree.selection()
+        for st in sel_tup:
+            box.insert(END, tree.item(st)['text'])
 
-        listbox_status = Listbox(frame,width=80)
-        listbox_status.grid(row=5, column=0, sticky='ns',pady=5,columnspan=4)
+    def del_box_item(self, box):
+        indexs = box.curselection()
+        for i in range(len(indexs)):
+            print box.get(indexs[i])
 
+    def select_ok(self, box):
+        pass
 
+    def process_directory(self, tree, parent, path):
+        for p in os.listdir(path):
+            abspath = os.path.join(path, p)
+            isdir = os.path.isdir(abspath)
+            oid = tree.insert(parent, 'end', text=p, open=False)
+            if isdir:
+                self.process_directory(tree, oid, abspath)
 
-        frame.pack()
+    def send_value(self):
+        print 'fwefe'
 
+    # def start_test(self):
+    # self.set_window_center(600, 300)
+
+    def yview(self, *args):
+        pass
+        # apply(self.b1.yview, args)
+        # apply(self.b2.yview, args)
 
     def load_data_box(self, parent):
         self.listbox_left = Listbox(parent, width=30, selectmode=EXTENDED)
@@ -120,14 +214,14 @@ class MainWin():
             self.listbox_left.delete(int(st))
 
 
-    def set_window_center(self, w, h):
+    def set_window_center(self, parent, w, h):
         curWidth = w  # self.root.winfo_reqwidth() # get current width
         curHeight = h  # self.root.winfo_height() # get current height
-        scnWidth, scnHeight = self.root.maxsize()  # get screen width and height
+        scnWidth, scnHeight = parent.maxsize()  # get screen width and height
         # now generate configuration information
         tmpcnf = '%dx%d+%d+%d' % (curWidth, curHeight,
                                   (scnWidth - curWidth) / 2, (scnHeight - curHeight) / 2)
-        self.root.geometry(tmpcnf)
+        parent.geometry(tmpcnf)
 
     def createWidgets(self, parent):
         # self.tb = ttk.Notebook(parent, height=200, width=300)
@@ -147,10 +241,10 @@ class MainWin():
         # dirs = os.listdir(_path)
         # for f in dirs:
         # all_f.append(f)
-        #     if os.path.isdir(os.path.join(_path, f)):
-        #         folders.append(f)
-        #     else:
-        #         files.append(f)
+        # if os.path.isdir(os.path.join(_path, f)):
+        # folders.append(f)
+        # else:
+        # files.append(f)
         #
         #
         # self.process_directory(root_node, path)
@@ -158,38 +252,38 @@ class MainWin():
         self.tree.grid(row=0, column=0, sticky='n')
         ysb.grid(row=0, column=1, sticky='ns')
         xsb.grid(row=1, column=0, sticky='ew')
-        #self.tb.grid(row=0, column=2)
+        # self.tb.grid(row=0, column=2)
         # self.tb.grid(row=0, column=0, sticky=N)
         self.tree.bind('<<TreeviewSelect>>', self.func)
 
-    def process_directory(self, parent, path):
+        # def process_directory(self, parent, path):
         # 遍历路径下的子目录
-        for p in path:
-            oid = self.tree.insert(parent, 'end', text=p, open=False)
+        # for p in path:
+        # oid = self.tree.insert(parent, 'end', text=p, open=False)
 
-    def func(self, event):
-        # 返回对象为Tuple
-        select = self.tree.selection()
-        txt = self.tree.item(select[0])['text']
-        # listb = Listbox(width=100)
-        # self.tb.add(listb, text=self.tree.item(select[0])['text'])
-        self.listbox_right.insert(END, txt)
+        # def func(self, event):
+        # # 返回对象为Tuple
+        #     select = self.tree.selection()
+        #     txt = self.tree.item(select[0])['text']
+        #     # listb = Listbox(width=100)
+        #     # self.tb.add(listb, text=self.tree.item(select[0])['text'])
+        #     self.listbox_right.insert(END, txt)
 
 
-        #select = select[0]
-        #if select == 'I002':  # and self.lock1 == 0:
+        # select = select[0]
+        # if select == 'I002':  # and self.lock1 == 0:
         # lable = Label(text='欢迎登陆！', fg='black')
 
-        #self.tb.add(lable, text='首页')
+        # self.tb.add(lable, text='首页')
         #self.lock1 = 1
 
 
     def show(self):
         self.base_form(self.root)
-        frame3 = Frame()
-        frame3.pack(fill=BOTH, expand=1)
-        self.createWidgets(frame3)
-        self.layout_widget()
+        # frame3 = Frame()
+        # frame3.pack(fill=BOTH, expand=1)
+        # self.createWidgets(frame3)
+        # self.layout_widget()
         self.root.mainloop()
 
 
